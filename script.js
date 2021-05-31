ef3bf2b44b12095c0bbae93502be9fd9fefb62b2
kotak364 <75615832+kotak364@users.noreply.github.com>
2021-05-30 19:48:49 -0800

    UPDATE

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+//VARIABLES
 const openMenu = document.querySelector(".open");
 const closeMenu = document.querySelector(".close");
 const navList = document.querySelector(".nav-list");
 
-console.log(openMenu);
-console.log(closeMenu);
-console.log(navList);
-
-
+//EVENT 1
 openMenu.addEventListener("click", () => {
-    navList.classList.add("active");
-})
+  navList.classList.add("active");
+});
 
-
+//EVENT 2
 closeMenu.addEventListener("click", () => {
-    navList.classList.remove("active");
-})
+  navList.classList.remove("active");
+});
+
 
 
